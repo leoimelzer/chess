8fdfa3ebd4f1ce509be7b9865010933ce8eac380
Leonardo Melzer <leoimelzer@gmail.com>
2022-06-06 09:16:37 -0300

    Starting to implement Board and Piece

--- src/br/com/leomelzer/application/Main.java
+++ src/br/com/leomelzer/application/Main.java
@@ -1,9 +1,12 @@
 package br.com.leomelzer.application;
 
+import br.com.leomelzer.boardgame.Board;
 
 public class Main {
 
     public static void main(String[] args) {
+        Board board = new Board(8,8);
 
+        
     }
 }
